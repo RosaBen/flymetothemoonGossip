073c8c9121e24de70e25507e5660057a5799ddbb
RosaBen <benchabane.rosa@outlook.com>
2025-06-02 12:45:59 +0200

    show all done

---
+++ app.rb
@@ -0,0 +1,14 @@
+# require 'csv'
+# # require 'pry'
+
+
+# require_relative 'lib/gossip.rb'
+# Gossip.init_csv
+# require_relative 'lib/controller.rb'
+# require_relative 'lib/router.rb'
+# require_relative 'lib/view.rb'
+
+
+
+
+# Router.new.perform
---
+++ lib/gossip.rb
@@ -0,0 +1,34 @@
+require 'csv'
+# require 'pry'
+
+class Gossip 
+    attr_accessor :author, :content
+
+        def initialize(author,content)
+        @author = author
+        @content = content
+    end
+
+
+    def self.init_csv
+            CSV.open("./db/gossip.csv", "w") do |header|
+                header << ["author", "content"]
+            end
+    end
+
+        def self.all
+        all_gossips = []
+        CSV.read("./db/gossip.csv", headers:true ).each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+    def save
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+    end
+end
+
+# binding pry
