189161dc030b02f8de5af918bbdb4835c07541ec
RosaBen <benchabane.rosa@outlook.com>
2025-06-02 11:16:52 +0200

    post WIP

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,15 @@
 
 
 class ApplicationController < Sinatra::Base
-get '/' do
-  erb :index
+  get '/' do
+    erb :index
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+  puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
 end
 end
